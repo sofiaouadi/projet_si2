a1a3565cd7859164e4f9cdd62d0e33458c0fad0c
sofiaouadi <133815006+sofiaouadi@users.noreply.github.com>
2025-01-19 02:01:37 +0100

    Update urls.py

--- personnel/urls.py
+++ personnel/urls.py
@@ -2,5 +2,13 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index, name='index'),
+    path('home/', views.home, name='home'),  
+    path('personnel/', views.liste_personnel, name='personnel'),  
+    path('employe/', views.employe, name='employe'),
+    path('ajouteremploye/', views.ajouter_employe, name='ajouter_employe'),
+    path('contrats/', views.liste_contrats, name='liste_contrats'),
+    path('contrats/ajoutercontrat/', views.ajouter_contrat, name='ajouter_contrat'),
+    path('contrats/modifiercontrat/<int:contrat_id>/', views.modifier_contrat, name='modifier_contrat'),
+    path('contrats/supprimercontrat/<int:contrat_id>/', views.supprimer_contrat, name='supprimer_contrat'),
+    path('', views.home, name='home'),
 ]
